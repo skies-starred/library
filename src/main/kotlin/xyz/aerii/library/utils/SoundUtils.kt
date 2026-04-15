828d719e436000539be21050e6757ed456fa7518
skies-starred <skies.starred@gmail.com>
2026-04-15 13:33:07 +0100

    internal(SoundUtils)

--- src/main/kotlin/xyz/aerii/library/utils/SoundUtils.kt
+++ src/main/kotlin/xyz/aerii/library/utils/SoundUtils.kt
@@ -2,13 +2,21 @@
 
 package xyz.aerii.library.utils
 
+import net.minecraft.client.resources.sounds.SimpleSoundInstance
+import net.minecraft.client.resources.sounds.SoundInstance
 import net.minecraft.resources.Identifier
 import net.minecraft.sounds.SoundEvent
+import net.minecraft.sounds.SoundSource
 import xyz.aerii.library.api.client
 
 @JvmOverloads
 fun SoundEvent.play(volume: Float = 1f, pitch: Float = 1f) {
     client.player?.playSound(this, volume, pitch)
+}
+
+@JvmOverloads
+fun SoundEvent.play(x: Double, y: Double, z: Double, volume: Float = 1f, pitch: Float = 1f) {
+    client.soundManager.play(SimpleSoundInstance(this, SoundSource.MASTER, volume, pitch, SoundInstance.createUnseededRandom(), x, y, z))
 }
 
 fun String.sound(): SoundEvent? {
