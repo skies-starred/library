e2726652704ce5552e520e6a085b3ceb0113aa0f
skies-starred <github@starred.foo>
2026-09-09 00:24:10 +0100

    fix(Client): java

--- src/main/kotlin/foo/starred/snowbird/api/Client.kt
+++ src/main/kotlin/foo/starred/snowbird/api/Client.kt
@@ -6,8 +6,15 @@ import net.minecraft.client.Minecraft
 import net.minecraft.client.gui.Font
 import net.minecraft.client.gui.components.ChatComponent
 
+@JvmField
 val client: Minecraft =
     Minecraft.getInstance()
+
+@JvmName("getClient2")
+@Deprecated("Use client.", ReplaceWith("client"))
+fun getClient(): Minecraft {
+    return client
+}
 
 val font: Font
     get() = client.font
